1a1d97c8ce0603fbd4ebe69d9d2d2f589da1f644
jeff <1105041+jeff-dh@users.noreply.github.com>
2021-05-22 22:10:03 +0200

    alternative solution for #172

--- solid/objects.py
+++ solid/objects.py
@@ -756,22 +756,47 @@ def disable(openscad_obj: OpenSCADObject) -> OpenSCADObject:
 # ===========================
 # = IMPORTING OPENSCAD CODE =
 # ===========================
+module_cache_by_name = {}
+module_cache_by_resolved_filename = {}
+
+def resolve_scad_file(scad_file):
+    scad_path = Path(scad_file)
+    if scad_path.is_absolute():
+        return scad_path
+
+    for p in _openscad_library_paths():
+        if (p / scad_path).exists():
+            return p / scad_path
+
+    return None
+
 def import_scad(scad_file_or_dir: PathStr) -> SimpleNamespace:
     '''
     Recursively look in current directory & OpenSCAD library directories for
         OpenSCAD files. Create Python mappings for all OpenSCAD modules & functions
     Return a namespace or raise ValueError if no scad files found
     '''
-    scad = Path(scad_file_or_dir)
-    candidates: List[Path] = [scad]
-    if not scad.is_absolute():
-        candidates = [d/scad for d in _openscad_library_paths()]
+    global module_cache_by_name, module_cache_by_resolved_filename
 
-    for candidate_path in candidates:
-        namespace = _import_scad(candidate_path)
-        if namespace is not None:
-            return namespace
-    raise ValueError(f'Could not find .scad files at or under {scad}. \nLocations searched were: {candidates}')
+    if scad_file_or_dir in module_cache_by_name.keys():
+        return module_cache_by_name[scad_file_or_dir]
+
+    resolved_scad = resolve_scad_file(scad_file_or_dir)
+
+    if resolved_scad in module_cache_by_name.keys():
+        return module_cache_by_resolved_filename[resolved_scad]
+
+    if not resolved_scad:
+        raise ValueError(f'Could not find .scad files at or under {scad_file_or_dir}.')
+
+    namespace = _import_scad(resolved_scad)
+
+    if not namespace:
+        raise ValueError(f'Could not import .scad file {resolved_scad.as_posix()}.')
+
+    module_cache_by_name[scad_file_or_dir] = namespace
+    module_cache_by_resolved_filename[resolved_scad] = namespace
+    return namespace
 
 def _import_scad(scad: Path) -> Optional[SimpleNamespace]:
     '''
