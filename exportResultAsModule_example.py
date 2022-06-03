a167914918d83b254bb6a0786ef1b5c4829dff1f
jeff <1105041+jeff-dh@users.noreply.github.com>
2022-06-03 14:56:37 +0200

    added children support

--- exportResultAsModule_example.py
+++ exportResultAsModule_example.py
@@ -2,20 +2,24 @@ from solid import *
 from exportReturnValueAsModuleExtension import exportReturnValueAsModule
 
 @exportReturnValueAsModule
-def blub(a):
+def blub(a, children):
     #this does not "compile", because you can't compare / evaluate a ScadValue
     #because it's value is unknown at python runtime
     #if a == 1:
     #    assert(False)
 
-    return circle(a)
+    return circle(a) + \
+           translate(0, 5, 0)(children(0)) + \
+           translate(0, -5, 0)(children(1))
 
 
 import time
+myChildModel = square(2)
+
 myModel = union()
-myModel += translate(10, 0, 0)(blub(3))
+myModel += translate(10, 0, 0)(blub(3, myChildModel, myChildModel))
 myModel += translate(20, 0, 0)(blub(4))
-myModel += translate(30, 0, 0)(blub(time.time()%10))
+myModel += translate(30, 0, 0)(blub(time.time()%10, myChildModel))
 
 #render scad code
 print(scad_render(myModel))
--- exportReturnValueAsModuleExtension.py
+++ exportReturnValueAsModuleExtension.py
@@ -1,5 +1,6 @@
 from solid.extensions.greedy_scad_interface import *
 from solid.core.utils import indent
+from solid.core.object_base import ObjectBase
 import inspect
 
 registeredModules = {}
@@ -15,6 +16,14 @@ from solid.core.extension_manager import default_extension_manager
 default_extension_manager.register_pre_render(lambda root : getCompileFunctionsHeader())
 
 def exportReturnValueAsModule(func):
+    def childrenToStr(args):
+        s = ""
+        for a in args:
+            s += a._render()
+            if s[-1] == "\n":
+                s = s[:-1]
+        return s
+
     def parametersToStr(args):
         s = ""
         for a in args:
@@ -22,16 +31,31 @@ def exportReturnValueAsModule(func):
         if len(s):
             #cut of trailing ","
             s = s[:-1]
+            if s[-1] == "\n":
+                s = s[:-1]
+            if s[-1] == ";":
+                s = s[:-1]
         return s
+
+    def childrenFunc(i):
+        return ScadValue(f"children({i});\n")
 
     if not func in registeredModules:
         argSpecs = inspect.getfullargspec(func).args 
-        parameters = [ScadValue(p) for p in argSpecs]
+        parameters = [ScadValue(p) for p in argSpecs if not p == 'children']
 
-        moduleCode = f"module {func.__name__}({parametersToStr(argSpecs)}){{\n"
-        moduleCode += indent(func(*parameters)._render())
+        moduleCode = f"module {func.__name__}({parametersToStr(parameters)}){{\n"
+        moduleCode += indent(func(*parameters, children=childrenFunc)._render())
         moduleCode += "}\n"
         registeredModules[func] = moduleCode
 
-    return lambda *args : ScadValue(f"{func.__name__}({parametersToStr(args)});")
+    def wrapper(*args):
+        parameters = [a for a in args if not issubclass(a.__class__, ObjectBase)]
+        children = [a for a in args if issubclass(a.__class__, ObjectBase)]
+
+        childrenStr = f"{{{childrenToStr(children)}}}" if len(children) else ""
+        parameterStr = parametersToStr(parameters)
+        return ScadValue(f"{func.__name__}({parameterStr})" + childrenStr + ";\n")
+
+    return wrapper
 
