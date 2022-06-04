dfa364b00d433596171764730bf3f5169d3b4afe
jeff <1105041+jeff-dh@users.noreply.github.com>
2022-06-04 02:03:23 +0200

    added sketch for an optimizer

---
+++ childOptimizerExtension.py
@@ -0,0 +1,90 @@
+from solid import *
+from solid.core.object_base import OpenSCADConstant, ObjectBase
+
+def childOptimizer(root):
+    nodeReferenceCount = {}
+    nodeParents = {}
+
+    #collect nodeRefereneCount and nodeParents dicts
+    def collectRefCountAndParents(node, parent=None, indent=""):
+        nonlocal nodeParents
+        nonlocal nodeReferenceCount
+
+        if not node in nodeParents:
+            nodeParents[node] = set()
+        if not node in nodeReferenceCount:
+            nodeReferenceCount[node] = 0
+
+        nodeReferenceCount[node] += 1
+
+        if parent != None:
+            nodeParents[node].add(parent)
+
+        #print(f"{indent}{node.name}:{id(node)}")
+        indent += "--"
+        for c in node.children:
+            collectRefCountAndParents(c, node, indent)
+
+    collectRefCountAndParents(root)
+
+
+    #extract the nodes we want to extract as children
+    childsToExtract = [n for n in nodeReferenceCount.keys() if nodeReferenceCount[n] > 1]
+    getChildId = lambda n : childsToExtract.index(n)
+
+    #replace the reference to the objects with calls to children(id)
+    for n in childsToExtract:
+        #print(f"{n.name} refCount: {nodeReferenceCount[n]}")
+        #print(f"!!!{getChildId(n)}")
+        parents = nodeParents[n]
+        #replace the references in each parent
+        for p in parents:
+            #this is to prevent a bug and causes issues!
+            #otherwise we get issues with nested children!
+            #TODO: I don't know what this means..... wrong algorithm???
+            #      do we need a recursiv approach?
+            if p in childsToExtract:
+                continue
+            while p.children.count(n) > 0:
+                idx = p.children.index(n)
+                p.children[idx] = OpenSCADConstant(f"children({getChildId(n)});\n")
+
+    #for n in childsToExtract:
+    #    print(f"child{getChildId(n)} {n._render()}")
+
+    #create wrapper object and fill it
+    mainModule = ObjectBase()
+    #add the mainModule wrapper
+    mainModule(OpenSCADConstant("module mainModule() {\n"))
+    #fill its body with the (modified) root node)
+    mainModule(root)
+    #...
+    mainModule(OpenSCADConstant("}\n"))
+
+    #the call to the mainModule wrapper
+    mainModule(OpenSCADConstant(f"mainModule(){{"))
+
+    #render all the childs
+    for n in childsToExtract:
+        mainModule(n)
+    #...
+    mainModule(OpenSCADConstant("}\n"))
+
+    return mainModule
+
+#register this extension
+from solid.core.extension_manager import default_extension_manager
+default_extension_manager.register_root_wrapper(childOptimizer)
+
+
+#test model 1
+m1 = cube(1) + sphere(2)
+m2 = circle(5) + cube(2) #m1
+model1 = m1 - m2 + m1.translateX(10)
+
+#test model 2
+c = cube(1)
+model2 = c + c
+
+#render
+print(model1)
