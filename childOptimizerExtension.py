ec05f379ae058c2fa0ea963dd04e90d393c7cbad
jeff <1105041+jeff-dh@users.noreply.github.com>
2022-06-05 01:45:25 +0200

    improved optimizer algorithm

--- childOptimizerExtension.py
+++ childOptimizerExtension.py
@@ -3,13 +3,21 @@ from solid.core.object_base import OpenSCADConstant, ObjectBase
 
 def childOptimizer(root):
     #print(root._render())
+    depthMap = []
     nodeReferenceCount = {}
     nodeParents = {}
 
     #collect nodeRefereneCount and nodeParents dicts
-    def collectRefCountAndParents(node, parent=None, indent=""):
+    def collectRefCountAndParents(node, parent=None, depth=0):
         nonlocal nodeParents
         nonlocal nodeReferenceCount
+
+        if len(depthMap) < depth+1:
+            assert(len(depthMap) == depth)
+            depthMap.append([])
+        assert(len(depthMap) >= depth+1)
+        depthMap[depth].append(node)
+        depth += 1
 
         if not node in nodeParents:
             nodeParents[node] = set()
@@ -25,55 +33,60 @@ def childOptimizer(root):
         if parent != None:
             nodeParents[node].add(parent)
 
-        #print(f"{indent}{node.name}:{id(node)}")
-        indent += "--"
+        #print(f"{depth * 2 * "-"}{node.name}:{id(node)}")
         for c in node.children:
-            collectRefCountAndParents(c, node, indent)
+            collectRefCountAndParents(c, node, depth)
 
     collectRefCountAndParents(root)
 
-
     #extract the nodes we want to extract as children
     childsToExtract = [n for n in nodeReferenceCount.keys() if nodeReferenceCount[n] > 1]
-    getChildId = lambda n : childsToExtract.index(n)
+    getChildId = lambda n : len(childsToExtract) - childsToExtract.index(n) - 1
 
     #replace the reference to the objects with calls to children(id)
-    for n in childsToExtract:
-        #print(f"{n.name} refCount: {nodeReferenceCount[n]}")
-        #print(f"!!!{getChildId(n)}")
-        parents = nodeParents[n]
-        #replace the references in each parent
-        for p in parents:
-            #this is to prevent a bug and causes issues!
-            #otherwise we get issues with nested children!
-            #TODO: I don't know what this means..... wrong algorithm???
-            #      do we need a recursiv approach?
-            if p in childsToExtract:
-                continue
-            while p.children.count(n) > 0:
-                idx = p.children.index(n)
-                p.children[idx] = OpenSCADConstant(f"children({getChildId(n)});\n")
-
     #for n in childsToExtract:
-    #    print(f"child{getChildId(n)} {n._render()}")
+    depthMap.reverse()
+    visitedNodes = []
+    for d in depthMap:
+        for n in d:
+            if n in visitedNodes:
+                continue
+            visitedNodes.append(n)
+            if not n in childsToExtract:
+                continue
+            #print(f"{n.name} refCount: {nodeReferenceCount[n]}")
+            #print(f"!!!{getChildId(n)}")
+            parents = nodeParents[n]
+            #replace the references in each parent
+            for p in parents:
+                while p.children.count(n) > 0:
+                    idx = p.children.index(n)
+                    p.children[idx] = OpenSCADConstant(f"children({getChildId(n)});\n")
 
     #create wrapper object and fill it
     mainModule = ObjectBase()
     #add the mainModule wrapper
-    mainModule(OpenSCADConstant("module mainModule() {\n"))
+    mainModule(OpenSCADConstant("module wrapperModule0() {\n"))
     #fill its body with the (modified) root node)
     mainModule(root)
     #...
     mainModule(OpenSCADConstant("}\n"))
 
-    #the call to the mainModule wrapper
-    mainModule(OpenSCADConstant(f"mainModule(){{"))
-
     #render all the childs
     for n in childsToExtract:
+        idx = childsToExtract.index(n)
+        mainModule(OpenSCADConstant(f"module wrapperModule{idx+1}() {{\n"))
+        #the call to the next wrapperModule
+        mainModule(OpenSCADConstant(f"wrapperModule{idx}(){{"))
+        #pass on all the childs
+        for i in range(len(childsToExtract) - idx - 1):
+            mainModule(OpenSCADConstant(f"children({i});"))
+        #render the "new child" in the chain
         mainModule(n)
-    #...
-    mainModule(OpenSCADConstant("}\n"))
+        mainModule(OpenSCADConstant("}}\n"))
+
+    #call the last wrapperModule
+    mainModule(OpenSCADConstant(f"wrapperModule{len(childsToExtract)}(){{}};\n"))
 
     return mainModule
 
