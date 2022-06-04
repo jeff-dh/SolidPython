90d08f474f3d5caf8dd8e37454f178533a91009f
jeff <1105041+jeff-dh@users.noreply.github.com>
2022-06-04 11:26:05 +0200

    bugfix

--- childOptimizerExtension.py
+++ childOptimizerExtension.py
@@ -15,7 +15,11 @@ def childOptimizer(root):
         if not node in nodeReferenceCount:
             nodeReferenceCount[node] = 0
 
-        nodeReferenceCount[node] += 1
+        if not parent in nodeParents[node]:
+            if parent != None:
+                nodeReferenceCount[node] += parent.children.count(node)
+            else:
+                nodeReferenceCount[node] += 1
 
         if parent != None:
             nodeParents[node].add(parent)
@@ -43,8 +47,8 @@ def childOptimizer(root):
             #otherwise we get issues with nested children!
             #TODO: I don't know what this means..... wrong algorithm???
             #      do we need a recursiv approach?
-            if p in childsToExtract:
-                continue
+            #if p in childsToExtract:
+            #    continue
             while p.children.count(n) > 0:
                 idx = p.children.index(n)
                 p.children[idx] = OpenSCADConstant(f"children({getChildId(n)});\n")
@@ -78,8 +82,9 @@ default_extension_manager.register_root_wrapper(childOptimizer)
 
 
 #test model 1
+c = cube(2)
 m1 = cube(1) + sphere(2)
-m2 = circle(5) + cube(2) #m1
+m2 = circle(5) + c + c #m1
 model1 = m1 - m2 + m1.translateX(10)
 
 #test model 2
