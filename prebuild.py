931533bd3a0c1ff8c6e96de8e9cdc623b2aa2821
ssuchter <sean-github@suchter.com>
2023-04-13 17:14:09 -0700

    Add check for BOSL2 submodule being initalized

--- prebuild.py
+++ prebuild.py
@@ -9,10 +9,22 @@ You should have poethepoet plugin installed before doing the above:
 
 % poetry self add 'poethepoet[poetry_plugin]'
 """
+import os
 import subprocess
+import sys
+
+
+def check_bosl2_submodule():
+    canary_file = "./solid2/libs/BOSL2/std.scad"
+    if not os.path.exists(canary_file):
+        print(f"Cannot find {canary_file} - probably you have not initalized the BOSL submodule")
+        print("You probably need to run 'git submodule init ; git submodule update'")
+        print("     (or clone with --recurse-submodules)")
+        sys.exit(1)
 
 
 def main():
+    check_bosl2_submodule()
     subprocess.check_call("./bosl2_generator.py", cwd="./solid2/extensions")
 
 
